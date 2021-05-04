7c279f90f0c10975ae4252f52fc81550c0b44f10
sumeshpandit <sumeshkpandit@gmail.com>
2021-05-04 13:52:34 +0530

    update

--- app/src/main/java/com/sumeshpandit/datafill/DisplayFragment.kt
+++ app/src/main/java/com/sumeshpandit/datafill/DisplayFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
-import androidx.navigation.NavArgs
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -15,7 +14,6 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DisplayFragment : Fragment() {
 
-    private lateinit var  button:FloatingActionButton
     private val args: DisplayFragmentArgs by navArgs()
 
     override fun onCreateView(
@@ -25,16 +23,14 @@ class DisplayFragment : Fragment() {
 
         val view= inflater.inflate(R.layout.fragment_display, container, false)
 
-        button=view.findViewById(R.id.backButton)
-
         val details = view.findViewById<TextView> (R.id.content);
 
         val fill=args.displayText
         details.text=fill
+        val salute="Have a nice day!"
+        Toast.makeText(context,salute, Toast.LENGTH_SHORT).show()
 
-        Toast.makeText(context,"Have a nice Day!", Toast.LENGTH_SHORT).show()
-
-        button.setOnClickListener {
+        view.findViewById<FloatingActionButton>(R.id.backButton).setOnClickListener {
 
             Navigation.findNavController(view).navigate(R.id.action_displayFragment_to_fillingFragment)
 
--- app/src/main/java/com/sumeshpandit/datafill/MainActivity.kt
+++ app/src/main/java/com/sumeshpandit/datafill/MainActivity.kt
@@ -2,15 +2,12 @@ package com.sumeshpandit.datafill
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val fillingFragment=FillingFragment()
-        supportFragmentManager.beginTransaction().replace(R.id.main_frame,fillingFragment).commit()
     }
 
 }
