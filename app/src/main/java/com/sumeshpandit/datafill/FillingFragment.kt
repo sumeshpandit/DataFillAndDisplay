938c4dacacb22d5e4fe91363752cda2864c7c5a5
sumeshpandit <sumeshkpandit@gmail.com>
2021-05-07 14:12:04 +0530

    updated data binding

--- app/src/main/java/com/sumeshpandit/datafill/FillingFragment.kt
+++ app/src/main/java/com/sumeshpandit/datafill/FillingFragment.kt
@@ -11,6 +11,14 @@ import com.sumeshpandit.datafill.databinding.FragmentFillingBinding
 
 class FillingFragment : Fragment() {
 
+    private var fName=""
+    private var lName=""
+    private var age=""
+    private var dept=""
+    private var email=""
+    private var male=false
+    private var female=false
+    private var other=""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,19 +29,19 @@ class FillingFragment : Fragment() {
 
         binding.submit.setOnClickListener {
 
-            binding.fname=binding.firstName.editText?.text.toString()
-            binding.lname=binding.lastName.editText?.text.toString()
-            binding.age=binding.pAge.editText?.text.toString()
-            binding.dept=binding.department.editText?.text.toString()
-            binding.email=binding.mail.editText?.text.toString()
-            binding.male=binding.Male.isChecked
-            binding.female=binding.Female.isChecked
-            binding.other=binding.additional.editText?.text.toString()
+            fName=binding.firstName.editText?.text.toString()
+            lName=binding.lastName.editText?.text.toString()
+            age=binding.pAge.editText?.text.toString()
+            dept=binding.department.editText?.text.toString()
+            email=binding.mail.editText?.text.toString()
+            male=binding.male.isChecked
+            female=binding.female.isChecked
+            other=binding.additional.editText?.text.toString()
 
-            val displayText="\n You are ${binding.fname} ${binding.lname}. \n \n You are ${binding.age} years old. \n \n" +
-                    " Your gender is ${if(binding.male==true) "Male" else if(binding.female==true) "Female" else " "} .\n \n Joining " +
-                    "details will be sent to: \n ${binding.email} \n \n You belong to ${binding.dept} department. \n " +
-                    "\n Additional Information:\n ${binding.other}\n"
+            val displayText="\n You are ${fName} ${lName}. \n \n You are ${age} years old. \n \n" +
+                    " Your gender is ${if(male==true) "Male" else if(female==true) "Female" else " "} .\n \n Joining " +
+                    "details will be sent to: \n ${email} \n \n You belong to ${dept} department. \n " +
+                    "\n Additional Information:\n ${other}\n"
 
             val action= FillingFragmentDirections.actionFillingFragmentToDisplayFragment(displayText)
 
